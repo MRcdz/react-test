519df42097d6bbb7934c736157284cbdcbef54b9
MRcdz <2463066771@qq.com>
2022-07-28 13:18:53 +0800

    m

--- src/App.js
+++ src/App.js
@@ -3,8 +3,58 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
-    </div>
+			<h1>react 和 bootstrap</h1>
+			<div className="card-box">
+				<div className="card">
+				  <img src="./tp.jpeg" className="card-img-top" alt="此处显示图片"/>
+				  <div className="card-body">
+				    <h5 className="card-title">Card title</h5>
+				    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#!" className="btn btn-primary">Go somewhere</a>
+				  </div>
+				</div>
+				<div className="card">
+				  <img src="./tp.jpeg" className="card-img-top" alt="此处显示图片"/>
+				  <div className="card-body">
+				    <h5 className="card-title">Card title</h5>
+				    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#!" className="btn btn-primary">Go somewhere</a>
+				  </div>
+				</div>
+				<div className="card">
+				  <img src="./tp.jpeg" className="card-img-top" alt="此处显示图片"/>
+				  <div className="card-body">
+				    <h5 className="card-title">Card title</h5>
+				    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#!" className="btn btn-primary">Go somewhere</a>
+				  </div>
+				</div>
+				<div className="card">
+				  <img src="./tp.jpeg" className="card-img-top" alt="此处显示图片"/>
+				  <div className="card-body">
+				    <h5 className="card-title">Card title</h5>
+				    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#!" className="btn btn-primary">Go somewhere</a>
+				  </div>
+				</div>
+				<div className="card">
+				  <img src="./tp.jpeg" className="card-img-top" alt="此处显示图片"/>
+				  <div className="card-body">
+				    <h5 className="card-title">Card title</h5>
+				    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#!" className="btn btn-primary">Go somewhere</a>
+				  </div>
+				</div>
+				<div className="card">
+				  <img src="./tp.jpeg" className="card-img-top" alt="此处显示图片"/>
+				  <div className="card-body">
+				    <h5 className="card-title">Card title</h5>
+				    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#!" className="btn btn-primary">Go somewhere</a>
+				  </div>
+				</div>
+			</div>
+		</div>
   );
 }
 
